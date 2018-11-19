5642907ddc92e8975aa41f1dd8ee9a2410784758
Eduard Tamsa <eduard.ioan.tamsa@gmail.com>
2018-11-19 02:03:43 -0800

    partial architecture 1

--- agregator/config_parser/config_parser.go
+++ agregator/config_parser/config_parser.go
@@ -1,1 +1,29 @@
-package config_parser
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+
+	// Open our jsonFile
+	jsonFile, err := os.Open("config.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened config.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+
+	fmt.Println(result)
+
+}
---
+++ probe/amqp_rpc_serv.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        // "strconv"
+        "os/exec"
+        
+        "github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+        if err != nil {
+                log.Fatalf("%s: %s", msg, err)
+        }
+}
+
+func execute(n string) string {
+        out, err :=  exec.Command("sh","-c",n).Output()
+        if err!=nil{
+                fmt.Printf("%s", err) 
+        }
+        return string(out)
+}
+
+func main() {
+        conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+        failOnError(err, "Failed to connect to RabbitMQ")
+        defer conn.Close()
+
+        ch, err := conn.Channel()
+        failOnError(err, "Failed to open a channel")
+        defer ch.Close()
+
+        q, err := ch.QueueDeclare(
+                "rpc_queue", // name
+                false,       // durable
+                false,       // delete when usused
+                false,       // exclusive
+                false,       // no-wait
+                nil,         // arguments
+        )
+        failOnError(err, "Failed to declare a queue")
+
+        err = ch.Qos(
+                1,     // prefetch count
+                0,     // prefetch size
+                false, // global
+        )
+        failOnError(err, "Failed to set QoS")
+
+        msgs, err := ch.Consume(
+                q.Name, // queue
+                "",     // consumer
+                false,  // auto-ack
+                false,  // exclusive
+                false,  // no-local
+                false,  // no-wait
+                nil,    // args
+        )
+        failOnError(err, "Failed to register a consumer")
+
+        forever := make(chan bool)
+
+        go func() {
+                for d := range msgs {
+                        // n, err := strconv.Atoi(string(d.Body))
+                        // failOnError(err, "Failed to convert body to integer")
+                        log.Printf(string(d.Body))
+                        // log.Printf(" [.] fib(%d)", n)
+                        response := execute(string(d.Body))
+
+                        err = ch.Publish(
+                                "",        // exchange
+                                d.ReplyTo, // routing key
+                                false,     // mandatory
+                                false,     // immediate
+                                amqp.Publishing{
+                                        ContentType:   "text/plain",
+                                        CorrelationId: d.CorrelationId,
+                                        Body:          []byte(response),
+                                })
+                        failOnError(err, "Failed to publish a message")
+
+                        d.Ack(false)
+                }
+        }()
+
+        log.Printf(" [*] Awaiting RPC requests")
+        <-forever
+}
